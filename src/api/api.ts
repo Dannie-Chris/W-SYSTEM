f774a2d9c11c632f85286ba4c3d82cf09bcb7334
Dannie-Chris <dunchris37@gmail.com>
2026-06-03 15:28:15 +0300

    Fix API baseURL for production deployment

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://welfinity-welfare-backend.onrender.com/api",
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
